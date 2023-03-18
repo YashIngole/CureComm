65078b8ca12e40490e936668705da69349cf19c8
YashIngole <yashingole1000@gmail.com>
2023-03-18 07:39:34 +0530

    before gradient

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,20 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:healthchats/body.dart';
 import 'package:healthchats/constants.dart';
 
 class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: BuildAppbar(),
+      body: body(),
+    );
+  }
+
+  AppBar BuildAppbar() {
+    return AppBar(
       backgroundColor: kbackgroundColor,
       elevation: 0,
       leading: IconButton(
@@ -15,8 +22,9 @@ class home extends StatelessWidget {
         onPressed: () {},
       ),
       actions: <Widget>[
-        IconButton(onPressed: () {}, icon: Icon(Icons.account_circle_outlined))
+        IconButton(onPressed: () {}, icon: Icon(Icons.account_circle_outlined)),
       ],
-    ));
+      title: const Text("Hello"),
+    );
   }
 }
