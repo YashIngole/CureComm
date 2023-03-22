dfad455898d68bec27bcc8b61ba03b6533f7c5e4
YashIngole <yashingole1000@gmail.com>
2023-03-22 19:52:08 +0530

    completely done the static contentof homepage screen

--- lib/body.dart
+++ lib/body.dart
@@ -40,32 +40,105 @@ class Body extends StatelessWidget {
                   suffixIcon: Icon(Icons.filter_list_sharp))),
         ),
       ),
-      // Container(
-      //   padding: EdgeInsets.all(kdefaultpadding),
-      //   child: Image.asset("assets/tile1.png"),
-      // ),
-      // Container(
-      //   padding: EdgeInsets.all(kdefaultpadding),
-      //   child: Image.asset("assets/tile2.png"),
-      //
+      //widget for tiles
       SizedBox(height: 20),
       Container(
         margin: EdgeInsets.symmetric(
-            horizontal: kdefaultpadding, vertical: kdefaultpadding - 20),
-        // color: kbackgroundColor2,
-        height: 200,
-        width: 480,
-        child: Image.asset("assets/tile1.png"),
+          horizontal: kdefaultpadding,
+          vertical: kdefaultpadding,
+        ),
+        height: 150,
+        width: 350,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(22),
+          boxShadow: [kdefaultshadow],
+          color: ktilecolor,
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              right: 0,
+              top: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: kdefaultpadding,
+                ),
+                height: 160,
+                width: 200,
+                child: Opacity(
+                  opacity: 1,
+                  child: Image.asset(
+                    "assets/pana1.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: kdefaultpadding,
+              bottom: kdefaultpadding,
+              top: kdefaultpadding + 35,
+              right: kdefaultpadding,
+              child: Text(
+                "Chat Rooms",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 198, 104, 104),
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+
       Container(
         margin: EdgeInsets.symmetric(
-            horizontal: kdefaultpadding, vertical: kdefaultpadding - 20),
-        // color: kbackgroundColor2,
-        height: 200,
-        width: 480,
-
-        child: Image.asset("assets/tile2.png"),
-      )
+          horizontal: kdefaultpadding,
+          vertical: kdefaultpadding,
+        ),
+        height: 150,
+        width: 350,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(22),
+          boxShadow: [kdefaultshadow],
+          color: ktilecolor,
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              right: 0,
+              top: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: kdefaultpadding,
+                ),
+                height: 160,
+                width: 200,
+                child: Opacity(
+                  opacity: 1,
+                  child: Image.asset(
+                    "assets/pana2.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: kdefaultpadding,
+              bottom: kdefaultpadding,
+              top: kdefaultpadding + 20,
+              right: kdefaultpadding,
+              child: Text(
+                "AI powered \n Chat Bot",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 198, 104, 104),
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     ]);
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,17 @@ const kbackgroundColor = Color.fromARGB(216, 249, 185, 177);
 const kdefaultpadding = 20.0;
 const ksearchcolor = const Color(0xfff08080);
 const kbackgroundColor2 = const Color(0xfffcc5ad);
+const ktilecolor = const Color(0xffFBC4AB);
+const kdefaultshadow = BoxShadow(
+    offset: Offset(0, 15),
+    blurRadius: 27,
+    color: Color.fromARGB(255, 240, 174, 174));
+
+const kdefaultshadow2 = BoxShadow(
+    color: Color.fromARGB(255, 255, 221, 170),
+    offset: Offset(0, 25),
+    blurRadius: 3,
+    spreadRadius: -10);
+
+const kdefaultgradient = LinearGradient(
+    colors: [kbackgroundColor2, Color.fromARGB(255, 238, 236, 232)]);
