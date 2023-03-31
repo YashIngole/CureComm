72700ef28a7e0f62d3c3696c20deaa54e27e9e3c
YashIngole <yashingole1000@gmail.com>
2023-03-31 17:56:59 +0530

    completed ui of drawer

--- lib/home.dart
+++ lib/home.dart
@@ -43,7 +43,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
         key: scaffoldKey,
         drawer: navbar(userName, email),
-        bottomNavigationBar: bottomnav(),
+        // bottomNavigationBar: bottomnav(),
 
         // appBar: BuildAppBar(),
         body: Stack(
--- lib/navigationbar.dart
+++ lib/navigationbar.dart
@@ -12,64 +12,90 @@ Drawer navbar(String userName, String email) {
 
   return Drawer(
     child: ListView(
+      padding: EdgeInsets.symmetric(vertical: 30),
       children: [
-        UserAccountsDrawerHeader(
-          accountName: Text(
-            userName,
-            style: TextStyle(
-                fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-          accountEmail:
-              Text(email, style: TextStyle(fontSize: 15, color: Colors.black)),
-          currentAccountPicture: CircleAvatar(
-            child: ClipOval(
-              child: Image.network(
-                "",
-                width: 90,
-                height: 90,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          decoration: BoxDecoration(color: ktilecolor),
+        Icon(
+          Icons.account_circle,
+          size: 150,
+          color: Colors.grey,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          userName,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontWeight: FontWeight.w800),
+        ),
+        Text(
+          email,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+        SizedBox(
+          height: 15,
         ),
         ListTile(
-          leading: Icon(Icons.favorite),
+          leading: Icon(
+            Icons.favorite,
+            color: Colors.pink,
+          ),
           title: Text("Favorites"),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.people),
+          leading: Icon(
+            Icons.people,
+            color: Colors.blue,
+          ),
           title: Text("Friends"),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.share),
+          leading: Icon(
+            Icons.share,
+            color: Colors.green,
+          ),
           title: Text("Share"),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.notifications),
+          leading: Icon(
+            Icons.notifications,
+            color: Colors.yellow,
+          ),
           title: Text("Requests"),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.settings),
+          leading: Icon(
+            Icons.settings,
+            color: Colors.black87,
+          ),
           title: Text("Settings"),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.description),
+          leading: Icon(
+            Icons.description,
+            color: Colors.brown,
+          ),
           title: Text("Policies"),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.info),
+          leading: Icon(
+            Icons.info,
+            color: Colors.deepOrange,
+          ),
           title: Text("About"),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.logout),
+          leading: Icon(
+            Icons.logout,
+            color: Colors.purple,
+          ),
           title: Text("Log out"),
           onTap: () {
             authService.signOut();
